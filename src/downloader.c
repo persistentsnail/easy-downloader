a426b84648d823960477ed94f91449739cab90c7
longzhiri <longzhiri@kingsoft.com>
2012-04-14 12:53:51 +0800

    save code

--- src/downloader.c
+++ src/downloader.c
@@ -60,6 +60,7 @@ char file_saved_def_path[PATH_MAX];
 #define ERR_CONNECT        -3
 #define ERR_IO_WRITE       -4
 #define ERR_IO_READ        -5
+#define ERR_IO_CREATE      -12
 #define ERR_RES_NOT_FOUND  -6
 
 #define ERR_OPEN_TMP_FILE  -7
@@ -384,40 +385,40 @@ static void download_progress(d_task_t *d_task, int bytes_recv, int bytes_total)
 	pthread_cond_signal(d_task->cond);
 }
 
-static void *download_part_entry(void *arg)
-{
-	part_info_t *part = (part_info_t *)arg;
-
-BEGIN_DOWN:
-{	int connfd = connect_server(&part->file->d_url);
+static int request_part_download_file(part_info_t *part)
+{	
+	int connfd = connect_server(&part->file->d_url);
+	int ret = ERR_FALSE;
+	fprintf(stderr, "download part id %d :  %d-%d \n", part->id, part->beg_pos, part->end_pos);
 	if (connfd < 0)
 	{
 		fprintf(stderr, "download part %d-%d connect to srv failed\n", part->beg_pos, part->end_pos);
-		return ERR_RET_VAL;
+		return ret;
 	}
+
 	{
-		char request[MAX_BUFFER_LEN];
-		char response[MAX_BUFFER_LEN];
+		char request[MAX_BUFFER_LEN + 1];
+		char response[MAX_BUFFER_LEN + 1];
 		int  nread;
-		char *body, *ptr;
+		char *ptr;
 		char status[4];
 		snprintf(request,MAX_BUFFER_LEN, CONNECT_STR_FMT_2, part->file->d_url.path,part->file->d_url.host,
 		part->file->d_url.host, part->beg_pos, part->end_pos);
 		if (strlen(request) != write_n_chars(connfd, request, strlen(request)))
 		{
 			close(connfd);
-			return ERR_RET_VAL;
+			return ERR_FALSE;
 		}
 		if ((nread = read(connfd, response, MAX_BUFFER_LEN)) < 0)
 		{
 			close(connfd);
-			return ERR_RET_VAL;
+			return ERR_IO_READ;
 		}
 		response[nread] = '\0';
 		if (!(ptr = strstr(response, "HTTP/1.")))
 		{
 			close(connfd);
-			return ERR_RET_VAL;
+			return ERR_FALSE;
 		}
 			
 		memcpy(status, ptr + strlen("HTTP/1.1 "), 3);
@@ -426,7 +427,7 @@ BEGIN_DOWN:
 		{
 			close(connfd);
 			fprintf(stderr, "request range content failed with bad status code %s\n", status);
-			return ERR_RET_VAL;
+			return ERR_FALSE;
 		}
 
 		if ((ptr = strstr(ptr, "Content-Length:")))
@@ -435,57 +436,114 @@ BEGIN_DOWN:
 			int flags;
 			char tmp_file_name[PATH_MAX];
 			FILE *fp;
+			int start_read_body;
+			char *body;
+
+			fd_set rfds;
 
 			ptr += strlen("Content-Length:");
 			sscanf(ptr, "%d", &range_len);
-			if (!(body = strstr(ptr, "\r\n\r\n")) || (range_len-1) != (part->end_pos - part->beg_pos))
+			if ((range_len-1) != (part->end_pos - part->beg_pos))
 			{
+				fprintf(stderr, "response content-length is not suitable value %d", range_len);
 				close(connfd);
-				return ERR_RET_VAL;
+				return ERR_FALSE;
 			}
-			body += 4;
-			nbody_read = strlen(body);
+
 			snprintf(tmp_file_name, PATH_MAX, part->d_task->tmp_file_name_fmt, part->id);
 			printf("tmp file name is %s\n", tmp_file_name);
-			if (!(fp = fopen(tmp_file_name, "a+")))
+			if (!(fp = fopen(tmp_file_name, "a")))
 			{
 				close(connfd);
-				return ERR_RET_VAL;
+				return ERR_FALSE;
 			}
-			fwrite(body, 1, nbody_read, fp);
-			download_progress(part->d_task, nbody_read, part->file->length);
-			
-			
-			nleft = range_len - nbody_read;
-			//flags = fcntl(connfd, F_GETFL, 0);
-			//fcntl(connfd, F_SETFL, flags |= O_NONBLOCK);
-			
-			while (nleft > 0)
-			{
-				nbody_read = read(connfd, response, MAX_BUFFER_LEN);
-				if (nbody_read < 0)
+
+			nbody_read      = 0;
+			body            = NULL;
+			nleft           = range_len - nbody_read;
+			start_read_body = 0;
+
+			flags = fcntl(connfd, F_GETFL, 0);
+			fcntl(connfd, F_SETFL, flags |= O_NONBLOCK);
+			FD_ZERO(&rfds);
+			FD_SET(connfd, &rfds);
+
+			while (1)
+			{	
+				// #if debug
+				char http_header[MAX_BUFFER_LEN+1];
+				// #endif
+				if (!start_read_body)
+				{
+					if (body = strstr(response, "\r\n\r\n"))
+					{
+						//#if debug
+						*body = 0;
+						strcpy(http_header, response);
+						//#endif
+						start_read_body = 1;
+						body += 4;
+						nbody_read = strlen(body);
+						
+					}
+				}
+				else
+				{
+					body = response;
+					nbody_read = nread;
+				}
+
+				if (start_read_body)
+				{
+					fwrite(body, 1, nbody_read, fp);
+					download_progress(part->d_task, nbody_read, part->file->length);
+					if ((nleft -= nbody_read) <= 0)
+						break;
+				}
+
+				fd_set active_fds = rfds;
+				int retval = select(connfd + 1, &active_fds, NULL, NULL, NULL);
+				if (retval == -1)
+				{
+					perror("select failed:");
+					ret = ERR_FALSE;
+					break;
+				}
+
+				nread = read(connfd, response, MAX_BUFFER_LEN);
+				if (nread < 0)
 				{
 					perror("read failed:");
-					fclose(fp);
-					close(connfd);
-					return ERR_RET_VAL;
+					ret = ERR_IO_READ;
+					break;
 				}
-				else if (nbody_read == 0)
+				else if (nread == 0)
 				{
-					close(connfd);
+					fprintf(stderr, "left %d bytes to recieve, but srv close, is anything wrong about srv?\n", nleft);
+					// #if debug
+					printf("http header is %s\n", http_header);
+					// #endif
 					part->beg_pos = part->end_pos - nleft + 1;
-					goto BEGIN_DOWN;
+					ret = 0;
+					break;
 				}
-				fwrite(response, 1, nbody_read, fp);
-				download_progress(part->d_task, nbody_read, part->file->length);
-				nleft -= nbody_read;
+
+				response[nread] = '\0';
 			}
+			close(connfd);
 			fclose(fp);
-			return 0;
+			return ret;
 		}
-		return ERR_RET_VAL;
+		return ERR_FALSE;
 	}
 }
+
+static void *download_part_entry(void *arg)
+{
+	part_info_t *part = (part_info_t *)arg;
+	int ret = 0;
+	while (ret == 0 && part->end_pos - part->beg_pos + 1 > 0)
+		ret = request_part_download_file(part);
 }
 
 static int merge_files(const char *dst_file, int dst_len, char src_files[MAX_PART_NUMBER][PATH_MAX], int *src_lens, int nsrcs)
@@ -557,7 +615,7 @@ static int merge_files(const char *dst_file, int dst_len, char src_files[MAX_PAR
 			munmap(src_buf, src_mapped_len);
 		}
 		close(src_fd);
-		unlink(src_files[i]);
+		/*unlink(src_files[i]);*/
 	}
 	munmap(dst_buf - dst_mem_beg_pos, dst_mapped_len);
 	close(dst_fd);
@@ -621,7 +679,10 @@ static void *dispatch_part_download(d_task_t *d_task, file_info_t *file,
 		char *p;
 		snprintf(file_full_path, PATH_MAX, "%s/%s", d_task->file_saved_path, file->filename);
 		if(merge_files(file_full_path, file->length, tmp_files_name, part_lens, parts) < 0)
+		{
 			fprintf(stderr, "merge files failed\n");
+			unlink(file_full_path);
+		}
 		if(p = strrchr(tmp_files_name[0], '/'))
 		{
 			*++p = 0;
@@ -636,6 +697,37 @@ static void *dispatch_part_download(d_task_t *d_task, file_info_t *file,
 		d_task->finished_callback(NULL);
 }
 
+static int _create_unique_file(d_task_t *d_task, file_info_t *pfile)
+{
+	// create unique downloading file
+	char file_full_path[PATH_MAX];
+	char *ptr, *ptr2;
+	int i;
+	snprintf(file_full_path, PATH_MAX, "%s/%s", d_task->file_saved_path, pfile->d_url.filename);
+	ptr = strrchr(file_full_path, '.');
+	if (!ptr)
+		ptr = file_full_path + strlen(file_full_path);
+	ptr2 = strrchr(pfile->d_url.filename, '.');
+	i = 0;
+	while (1)
+	{
+		int ret = open(file_full_path, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+		if (ret == -1 && errno == EEXIST)
+			sprintf(ptr, "(%d)%s", ++i, (ptr2 ? ptr2 : ""));
+		else if (ret == -1)
+			return ERR_IO_CREATE;
+		else
+		{
+			close(ret);
+			break;
+		}
+	}
+	if (ptr = strrchr(file_full_path, '/'))
+		strcpy(pfile->filename, ++ptr);
+	return 0;
+}
+
+
 static void *download_entry(void *arg)
 {
 	d_task_t *d_task = (d_task_t *)arg;
@@ -643,11 +735,10 @@ static void *download_entry(void *arg)
 	char sql_buf[1024];
 	char real_url[MAX_URL_LEN];
 	char tmp_file_name_fmt[PATH_MAX];
-	char file_full_path[PATH_MAX];
 
 
 	int ret, i;
-	char *ptr, *ptr2;
+	char *ptr;
 
 	if (request_download_file(d_task->url, &file, real_url) == 0)
 	{
@@ -671,24 +762,11 @@ static void *download_entry(void *arg)
 		}
 
 		// create unique downloading file
-		snprintf(file_full_path, PATH_MAX, "%s/%s", d_task->file_saved_path, file.d_url.filename);
-		ptr = strrchr(file_full_path, '.');
-		if (!ptr)
-			ptr = file_full_path + strlen(file_full_path);
-		ptr2 = strrchr(file.d_url.filename, '.');
-		i = 0;
-		while (1)
+		if (_create_unique_file(d_task, &file) != 0)
 		{
-			ret = open(file_full_path, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-			if (ret == -1 && errno == EEXIST)
-				sprintf(ptr, "(%d)%s", ++i, (ptr2 ? ptr2 : ""));
-			else if (ret == -1)
-				return ERR_RET_VAL;
-			else
-				break;
+			fprintf(stderr, "error when create download file\n");
+			return ERR_RET_VAL;
 		}
-		if (ptr = strrchr(file_full_path, '/'))
-			strcpy(file.filename, ++ptr);
 		// create unique tmp file dir
 		strcpy(tmp_file_name_fmt, file.filename);
 		ptr = tmp_file_name_fmt + strlen(tmp_file_name_fmt);
@@ -710,9 +788,6 @@ static void *download_entry(void *arg)
 				real_url, d_task->file_saved_path, file.length, d_task->tmp_file_name_fmt, parts,
 				per_part_len, last_part_len);
 
-//#if debug
-		printf("sql string is %s\n", sql_buf);
-//endif
 		db_execute(d_task->dm->db_key, sql_buf, NULL);
 
 		dispatch_part_download(d_task, &file, per_part_len, last_part_len, parts);
@@ -768,6 +843,12 @@ static void *recover_entry(void *arg)
 	
 	sqlite3_free_table(results);
 
+	// create unique downloading file
+	if (_create_unique_file(d_task, &file) != 0)
+	{
+		fprintf(stderr, "error when create download file\n");
+		return ERR_RET_VAL;
+	}
 	dispatch_part_download(d_task, &file, per_part_len, last_part_len, parts);
 }
 
@@ -861,8 +942,7 @@ static void get_ith_breakpoint(d_breakpoint_t *bp, char **results, int i)
 	char tmp_file_name_fmt[PATH_MAX];
 	int parts, j;
 	// file_saved_path file_name
-	strncpy(bp->file_name, results[i+1], PATH_MAX);
-	strcat(bp->file_name, results[i]);
+	snprintf(bp->file_name, PATH_MAX, "%s/%s", results[i+1], results[i]);
 	i += 2;
 
 	// file_length
